a1d03114846e9e6d139e868c3bbfdad69df1c286
Habib Naderi <habib@cs.auckland.ac.nz>
2015-03-13 12:42:36 +1300

    Added whitelist and blacklist for functions to make it easier to change in the future.

--- ij/macro/Headless.java
+++ ij/macro/Headless.java
@@ -4,37 +4,33 @@
 package ij.macro;
 
 import static ij.macro.MacroConstants.DIALOG;
-import static ij.macro.MacroConstants.GET_PIXEL;
-import static ij.macro.MacroConstants.PRINT;
-import static ij.macro.MacroConstants.PUT_PIXEL;
 import static ij.macro.MacroConstants.SET_PIXEL;
+import static ij.macro.MacroConstants.PUT_PIXEL;
+import static ij.macro.MacroConstants.PRINT;
 
 /**
  *
  * @author hnad002
  */
 public class Headless {
-        // returns true, if the predefined function is allowed to be called in batch mode.
-        static boolean checkPredefined(int type) {
-            switch(type) {
-                case GET_PIXEL:
-                case SET_PIXEL:
-                case PUT_PIXEL:
-                case PRINT:
+        static final int predefinedWhiteList[] = new int [] {SET_PIXEL, PUT_PIXEL, PRINT};
+        static final int getStringBlackList[] = new int [] {DIALOG};
+        
+        static private boolean isInList(int[] list, int item) {
+            for (int e: list) {
+                if (e == item)
                     return true;
-                default:
-                    return false;
             }
+            return false;
+        }
+               
+        static boolean checkPredefined(int type) {
+            return isInList(predefinedWhiteList, type);
         }
         
         // returns true, if getString type function is allowed to be called in batch mode.
         static boolean checkGetString(int type) {
-            switch(type) {
-                case DIALOG:
-                    return false;
-                default:
-                    return true;
-            }            
+            return !isInList(getStringBlackList, type);
         }      
         
         // generates exception for headless version related errors.
